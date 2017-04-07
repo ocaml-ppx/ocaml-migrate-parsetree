34c12aaafffc0874e16082b5958d35362a20f4cb
Frédéric Bour <frederic.bour@lakaban.net>
2017-04-07 13:08:22 +0200

    Expose rewriters (#16)
    * expose Migrate_parsetree.Driver.rewrite_{signature,structure}
    
    * Add an 'extras' field to Driver.config

--- src/migrate_parsetree_driver.mli
+++ src/migrate_parsetree_driver.mli
@@ -2,13 +2,28 @@ open Migrate_parsetree_versions
 
 (** {1 State a rewriter can access} *)
 
+type extra = ..
+
 type config = {
   tool_name       : string;
   include_dirs    : string list;
   load_path       : string list;
   debug           : bool;
   for_package     : string option;
+  (** Additional parameters that can be passed by a caller of
+      [rewrite_{signature,strucutre}] to a specific register rewriter. *)
+  extras          : extra list;
 }
+
+val make_config
+  :  tool_name:string
+  -> ?include_dirs:string list
+  -> ?load_path:string list
+  -> ?debug:bool
+  -> ?for_package:string
+  -> ?extras:extra list
+  -> unit
+  -> config
 
 type cookies
 
@@ -40,3 +55,35 @@ val run_as_ast_mapper : string list -> Ast_mapper.mapper
 val run_as_ppx_rewriter : unit -> 'a
 
 val run_main : unit -> 'a
+
+(** {1 Manual mapping} *)
+
+type some_signature =
+  | Sig : (module Migrate_parsetree_versions.OCaml_version with
+            type Ast.Parsetree.signature = 'concrete) * 'concrete -> some_signature
+
+type some_structure =
+  | Str : (module Migrate_parsetree_versions.OCaml_version with
+            type Ast.Parsetree.structure = 'concrete) * 'concrete -> some_structure
+
+val migrate_some_signature
+  :  'version ocaml_version
+  -> some_signature
+  -> 'version get_signature
+
+val migrate_some_structure
+  :  'version ocaml_version
+  -> some_structure
+  -> 'version get_structure
+
+val rewrite_signature
+  :  config
+  -> 'version ocaml_version
+  -> 'version get_signature
+  -> some_signature
+
+val rewrite_structure
+  :  config
+  -> 'version ocaml_version
+  -> 'version get_structure
+  -> some_structure
