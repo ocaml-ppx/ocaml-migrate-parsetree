a2e269d208f1c388a9249e00e817a2b00a7744f0
hhugo <hugo.heuzard@gmail.com>
2017-10-21 14:51:13 +0100

    Update magic number for 4.06

--- src/ast_406.ml
+++ src/ast_406.ml
@@ -2969,8 +2969,8 @@ module Outcometree = struct
 end
 
 module Config = struct
-  let ast_impl_magic_number = "Caml1999M020"
-  let ast_intf_magic_number = "Caml1999N018"
+  let ast_impl_magic_number = "Caml1999M022"
+  let ast_intf_magic_number = "Caml1999N022"
 end
 
 let map_signature mapper = mapper.Ast_mapper.signature mapper
