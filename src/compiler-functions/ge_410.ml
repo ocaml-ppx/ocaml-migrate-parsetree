abfd01932ac987b30c33bfccf7e90b956f0679fd
Carl Eastlund <ceastlund@janestreet.com>
2020-07-10 10:29:52 -0400

    removed unnecessary compiler function wrappers

--- src/compiler-functions/ge_410.ml
+++ src/compiler-functions/ge_410.ml
@@ -4,16 +4,4 @@ let error_of_exn exn =
   | Some `Already_displayed -> None
   | None -> None
 
-let get_load_paths () =
-  Load_path.get_paths ()
-
-let load_path_init l =
-  Load_path.init l
-
-let get_unboxed_types () =
-  !Clflags.unboxed_types
-
-let set_unboxed_types b =
-  Clflags.unboxed_types := b
-
 let may_map = Option.map
