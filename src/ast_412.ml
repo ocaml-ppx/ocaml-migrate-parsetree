6c6dde534337c649f87511238af26324aa667430
Carl Eastlund <ceastlund@janestreet.com>
2020-09-23 17:23:13 -0400

    copy magic numbers
    Signed-off-by: Carl Eastlund <ceastlund@janestreet.com>

--- src/ast_412.ml
+++ src/ast_412.ml
@@ -1016,6 +1016,6 @@ module Parsetree = struct
 end
 
 module Config = struct
-  let ast_impl_magic_number = "Caml1999M028"
-  let ast_intf_magic_number = "Caml1999N028"
+  let ast_impl_magic_number = "Caml1999M027"
+  let ast_intf_magic_number = "Caml1999N027"
 end
